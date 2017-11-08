ed6ba8da5d7746982327e2aa7e649137d0b06664
ftatp <ftatp5901@gmail.com>
2017-11-08 10:44:51 +0900

    Do all commands

--- src/commands.c
+++ src/commands.c
@@ -33,27 +33,33 @@ static int is_built_in_command(const char* command_name)
 */
 int evaluate_command(int n_commands, struct single_command (*commands)[512])
 {
-	if (n_commands > 0) {
-		struct single_command* com = (*commands);
+	//if (n_commands > 0) {
+	struct single_command* com = (*commands);
 
-		assert(com->argc != 0);//if com->argc == 0, abort
+	for(int i = 0; i < n_commands; i++) {
 
-		int built_in_pos = is_built_in_command(com->argv[0]);
+		assert((com + i)->argc != 0);//if com->argc == 0, abort
+		
+		outfd = dup(1);
+		close(1);
+
+		int built_in_pos = is_built_in_command((com + i)->argv[0]);
 		if (built_in_pos != -1) {
-			if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
-				if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
-					fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
+			if (built_in_commands[built_in_pos].command_validate((com + i)->argc, (com + i)->argv)) {
+				if (built_in_commands[built_in_pos].command_do((com + i)->argc, (com + i)->argv) != 0) {
+					// TODO: Implements ~ 
+					fprintf(stderr, "%s: Error occurs\n", (com + i)->argv[0]);
 				}
 			} 
 			else {
-				fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
+				fprintf(stderr, "%s: Invalid arguments\n", (com + i)->argv[0]);
 				return -1;
 			}
 		} 
-		else if (strcmp(com->argv[0], "") == 0) {
+		else if (strcmp((com + i)->argv[0], "") == 0) {
 			return 0;
 		} 
-		else if (strcmp(com->argv[0], "exit") == 0) {
+		else if (strcmp((com + i)->argv[0], "exit") == 0) {
 			return 1;
 		} 
 		else {
@@ -71,21 +77,23 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 					"/usr/bin/",
 					"/bin/",
 					"/usr/sbin/",
-					"/sbin/"
+					"/sbin/",
+					""
 				};
 
-				for(int i = 0; i < 5; i++)
+				for(int j = 0; j < 6; j++)
 				{
-					char* command_path = (char *)malloc(strlen(path[i]) * sizeof(char) + strlen(com->argv[0]));
-					strcpy(command_path, path[i]);
-					strcat(command_path, com->argv[0]);
-					execv(command_path, com->argv);
+					char* command_path = (char *)malloc(strlen(path[j]) * sizeof(char) + strlen((com + i)->argv[0]));
+					strcpy(command_path, path[j]);
+					strcat(command_path, (com + i)->argv[0]);
+					execv(command_path, (com + i)->argv);
 					free(command_path);
 				}
 				fprintf(stderr, "Command not found\n");
-				return -1;
+				exit(0);
 			}
 			else{
+				//for background process, need to be modified to not wait when & comes in the end of the command
 				wait(NULL);
 				printf("Child complete\n");
 			}
