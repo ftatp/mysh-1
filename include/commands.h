7b4303eb4232a4583c3506e5641ea661e40c963c
ftatp <ftatp5901@gmail.com>
2017-11-11 17:38:33 +0900

    Modified for pipe function - all done except reading

--- include/commands.h
+++ include/commands.h
@@ -11,4 +11,9 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512]);
 
 void free_commands(int n_commands, struct single_command (*commands)[512]);
 
+int execute(struct single_command*);
+
+char* get_input_from_parent(int*);
+void put_output_to_child(int*, char*);
+
 #endif // MYSH_COMMANDS_H_
